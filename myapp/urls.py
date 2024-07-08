3382b87f9fce8edb827b6479e8e28d556c91c86f
AliIbrahimCesur <alicesur.ytu@gmail.com>
2024-07-08 14:50:54 +0200

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+# myapp/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+
+]
---
+++ myapp/views.py
@@ -0,0 +1,57 @@
+# myapp/views.py
+
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.decorators import action
+
+class BookPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = BookPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'author']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        queryset = self.filter_queryset(self.get_queryset())
+        title_query = request.query_params.get('title', None)
+        author_query = request.query_params.get('author', None)
+        print("queryset:",queryset)
+
+        if title_query:
+            queryset = queryset.filter(title__icontains=title_query)
+        if author_query:
+            queryset = queryset.filter(author__icontains=author_query)
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
